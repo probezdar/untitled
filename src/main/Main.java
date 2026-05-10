e362f5e30bccf22e6b958b4c5bc68cba6e80c0f2
Kirill <kirill031312@mail.ru>
2026-05-10 15:46:14 +0300

    10.05.26

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
-void main() {
+public static void main(String[] args) {
     SwingUtilities.invokeLater(() -> {
         List<Vulnerability> modules = List.of(
                 new SQLInjection(),
@@ -21,11 +21,15 @@ void main() {
             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 
             Font defaultFont = new Font("Segoe UI", Font.PLAIN, 16);
-            UIManager.put("Button font", defaultFont);
+            UIManager.put("Button.font", defaultFont);
             UIManager.put("Label.font", defaultFont);
             UIManager.put("TextArea.font", new Font("Monospaced", Font.PLAIN,16));
             UIManager.put("List.font",defaultFont);
-        } catch (Exception e) {e.printStackTrace();}
+        } catch (UnsupportedLookAndFeelException e) {
+            System.err.println("Не удалось установить LookAndFeel:" + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Ошибка инициализации UI:" + e.getMessage());
+        }
         new Window(modules);
     });
 }
